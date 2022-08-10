35167fb8bbe4410aafe4e948f72f6aa9e148cb63
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-10 06:00:08 +0530

    update code

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-var _ = require('lodash');
+const _ = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -47,15 +46,14 @@ app.get("/compose", function(req, res) {
 app.get("/posts/:specpost", function(req, res) {
 
   for (var i = 0; i < posts.length; i++) {
-    if (_.lowerCase(posts[i].title) ==_.lowerCase(req.params.specpost)) {
-      res.render("post",{
-        title : posts[i].title,
-        body : posts[i].body
+    if (_.lowerCase(posts[i].title) == _.lowerCase(req.params.specpost)) {
+      res.render("post", {
+        title: posts[i].title,
+        body: posts[i].body
       })
     }
   }
 });
-
 
 app.post("/compose", function(req, res) {
   const post = {
@@ -67,14 +65,6 @@ app.post("/compose", function(req, res) {
   res.redirect("/");
 
 })
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
