08b51d9bbd122b8142a94e37bb6efa800c4b5198
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-19 23:41:06 +0530

    add mongoDB database

--- app.js
+++ app.js
@@ -1,41 +1,123 @@
+//DON'T CHANGE THESE...
+
 const express = require("express");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require('lodash');
-
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const posts = [];
-
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+mongoose.connect('mongodb+srv://rakshit:chitkararakshit@cluster0.q0egtms.mongodb.net/dailyjournalDB');
+
+
+
+
+
+//SCHEMA
+
+const itemSchema = new mongoose.Schema({
+  title: "String",
+  content: "String"
+});
+const Item = mongoose.model("Item", itemSchema);
+
+
+
+
+//GET FUNCTION FOR LANDING PAGE
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    content: homeStartingContent,
-    posts: posts
-  });
+  res.render("landing")
 });
 
-app.get("/about", function(req, res) {
-  res.render("about", {
-    content: aboutContent
+
+
+//POST FUNCTION WHEN WE COMPOSE A NEW THING
+
+app.post("/compose", function(req, res) {
+  const newtitle = req.body.newt;
+  const newcont = req.body.newm;
+  const newday = new Item({
+    title: newtitle,
+    content: newcont
   });
+  newday.save();
+  res.redirect("/home");
+});
+
+
+//GET FUNCTION FOR WHEN HOME IS CALLED
+
+app.get("/home", function(req, res) {
+  Item.find({}, function(err, items) {
+    if (err) {
+      console.log(err);
+    } else if (items.length == 0) {
+      res.render("landing")
+    } else {
+      res.render("home", {
+        posts: items
+      });
+    }
+  })
+});
+
+
+//POST FUNCTION WHEN SOMEONE CLICKS READ MORE.
+
+app.get("/posts/:specpost", function(req, res) {
+
+  Item.find({}, function(err, items) {
+    if (err) {
+      console.log(err);
+    } else {
+      for (var i = 0; i < items.length; i++) {
+        if (items[i]._id == req.params.specpost) {
+          res.render("post", {
+            title: items[i].title,
+            body: items[i].content,
+            id: items[i]._id
+          })
+        }
+      }
+    }
+  })
+});
+
+
+
+
+
+//POST FUNCTION WHEN SOMEONE CLICKS DELETE.
+app.post("/delete", function(req, res) {
+  const tbd = req.body.delete;
+
+  Item.deleteOne({
+    _id: tbd
+  }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/home");
+    }
+  })
+});
+
+
+
+
+
+
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact", {
-    content: contactContent
-  });
+  res.render("contact"  );
 });
 
 app.get("/compose", function(req, res) {
@@ -43,28 +125,7 @@ app.get("/compose", function(req, res) {
 });
 
 
-app.get("/posts/:specpost", function(req, res) {
 
-  for (var i = 0; i < posts.length; i++) {
-    if (_.lowerCase(posts[i].title) == _.lowerCase(req.params.specpost)) {
-      res.render("post", {
-        title: posts[i].title,
-        body: posts[i].body
-      })
-    }
-  }
-});
-
-app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.newt,
-    body: req.body.newm
-  }
-
-  posts.push(post);
-  res.redirect("/");
-
-})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
